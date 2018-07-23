0f1a42168c5e2557f379949e1a34fae5d37240c9
UkroP911 <ukrop911@gmail.com>
2018-07-23 11:41:37 +0300

    bugfix(header): delete comments

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -3,19 +3,8 @@ import React, {Component} from 'react'
 import Header from '../../components/Header';
 
 class HeaderContainer extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            showMenu: false,
-        }
-    }
     navHandler = () => {
-
-        // if(this.state.showMenu === true){
-            document.querySelector('.header__nav').classList.toggle('open-nav')
-        // } else {
-            // document.querySelector('.header__nav').classList.remove('open-nav')
-        // }
+        document.querySelector('.header__nav').classList.toggle('open-nav')
     }
 
     render(){
